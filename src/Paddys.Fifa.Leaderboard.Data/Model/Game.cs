0d1cb86afd654494df698ec4e63f71363b03aaf8
chrishey <chrishey87@hotmail.com>
2015-01-29 20:46:03 +0000

    Added data project and some basic models

---
+++ src/Paddys.Fifa.Leaderboard.Data/Model/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace Paddys.Fifa.Leaderboard.Data.Model
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+        public int Player1Score { get; set; }
+        public int Player2Score { get; set; }
+    }
+}
---
+++ src/Paddys.Fifa.Leaderboard.Data/Model/Player.cs
@@ -0,0 +1,10 @@
+﻿namespace Paddys.Fifa.Leaderboard.Data.Model
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public double Coefficient { get; set; }
+    }
+}
