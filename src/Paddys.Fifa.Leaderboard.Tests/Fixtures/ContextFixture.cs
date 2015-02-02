2d120232c82798777b62a7b5e733dca7e355d874
chrishey <chrishey87@hotmail.com>
2015-02-02 13:48:52 +0000

    1st test to check game can be added and saved

--- src/Paddys.Fifa.Leaderboard.Tests/Fixtures/ContextFixture.cs
+++ src/Paddys.Fifa.Leaderboard.Tests/Fixtures/ContextFixture.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using Paddys.Fifa.Leaderboard.Data.Model;
+using Paddys.Fifa.Leaderboard.Interfaces.Data;
 
 namespace Paddys.Fifa.Leaderboard.Tests.Fixtures
 {
-    public class ContextFixture : IDisposable
+    public class ContextFixture : IDisposable, IContext
     {
-        public IList<Game> Games { get; private set; }
-        public IList<Game> Players { get; private set; } 
+        public IList<Game> Games { get; set; }
+        public IList<Player> Players { get; set; } 
 
         public ContextFixture()
         {
@@ -19,11 +20,18 @@ namespace Paddys.Fifa.Leaderboard.Tests.Fixtures
         public void Dispose()
         {
             Games = null;
+            Players = null;
         }
 
-        private IList<Game> GeneratePlayers()
+        private IList<Player> GeneratePlayers()
         {
-            throw new NotImplementedException();
+            return new List<Player>
+            {
+                new Player {Id = 1, FirstName = "Chris", Surname = "Hey", Coefficient = 0.01},
+                new Player {Id = 2, FirstName = "Dave", Surname = "Saunders", Coefficient = 0.1},
+                new Player {Id = 3, FirstName = "Steve", Surname = "Robbins", Coefficient = 0.5},
+                new Player {Id = 4, FirstName = "Paddy", Surname = "Halle", Coefficient = 1}
+            };
         }
     }
 }
