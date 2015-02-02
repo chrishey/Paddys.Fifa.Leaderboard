c06dad3f0c2f7e3319aa9cf922190f70e0694f37
chrishey <chrishey87@hotmail.com>
2015-02-02 12:43:49 +0000

    Added leaderboard class exposing a pre-ordered list

---
+++ src/Paddys.Fifa.Leaderboard.Data/Model/Leaderboard.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace Paddys.Fifa.Leaderboard.Data.Model
+{
+    public class Leaderboard
+    {
+        public IOrderedEnumerable<Player> Rankings { get; set; } 
+    }
+}
