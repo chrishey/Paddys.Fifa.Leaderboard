9df6687bc0acd94cc41a9e99357f846559a63174
chrishey <chrishey87@hotmail.com>
2015-02-05 08:53:49 +0000

    Generic cache interface

---
+++ src/Paddys.Fifa.Leaderboard.Interfaces/Data/ICache.cs
@@ -0,0 +1,8 @@
+﻿namespace Paddys.Fifa.Leaderboard.Interfaces.Data
+{
+    public interface ICache<T> where T : class
+    {
+        T Get(string key);
+        void Put(string key, T value);
+    }
+}
