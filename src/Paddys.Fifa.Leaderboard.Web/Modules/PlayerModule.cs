8b2cb748a550eacb51cc4e90d34c73443e0d9028
pathalle006 <patrick.halle@mccann.com>
2015-02-22 21:58:30 +0000

    Started implementing adding a player

---
+++ src/Paddys.Fifa.Leaderboard.Web/Modules/PlayerModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.ModelBinding;
+using Nancy.Validation;
+using Paddys.Fifa.Leaderboard.ViewModels;
+
+namespace Paddys.Fifa.Leaderboard.Modules
+{
+	public class PlayerModule : NancyModule
+	{
+		public PlayerModule() : base("/player")
+		{
+			Get["/addplayer"] = _ => View["Shared/_AddPlayer"];
+
+			Post["/addplayer"] = _ =>
+				{
+					var viewModel = this.BindAndValidate<PlayerDetails>();
+					var model = this.Bind<PlayerDetails>();
+					var result = this.Validate(model);
+
+					if (!result.IsValid)
+					{
+						return "Error";
+					}
+
+					return View["Shared/_AddPlayer"];
+				};
+		}
+	}
+}
---
+++ src/Paddys.Fifa.Leaderboard.Web/ViewModels/PlayerDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Paddys.Fifa.Leaderboard.ViewModels
+{
+	public class PlayerDetails
+	{
+		[Required]
+		public string	FirstName	{ get; set; }
+		
+		[Required]
+		public string	Surname		{ get; set; }
+		
+		[Required, Range(1, 10)]
+		public int		Level		{ get; set; }
+	}
+}
