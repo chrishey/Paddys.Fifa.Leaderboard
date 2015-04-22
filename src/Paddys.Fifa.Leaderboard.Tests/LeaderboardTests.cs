162764915ae23940a7b0b19c572e3d1c8a6c790a
DRL\heych <D@rr3nH3lm>
2015-04-22 08:45:40 +0100

    Remove the not required Models project, enable nuget restore and add a test class for leaderboard

---
+++ src/Paddys.Fifa.Leaderboard.Tests/LeaderboardTests.cs
@@ -0,0 +1,18 @@
+﻿using Paddys.Fifa.Leaderboard.Tests.Fixtures;
+using Xunit;
+
+namespace Paddys.Fifa.Leaderboard.Tests
+{
+    public class LeaderboardTests : IClassFixture<ContextFixture>
+    {
+        [Fact]
+        public void DisplayLeaderboard()
+        {
+            // Given
+
+            // When
+
+            // Then
+        }
+    }
+}
