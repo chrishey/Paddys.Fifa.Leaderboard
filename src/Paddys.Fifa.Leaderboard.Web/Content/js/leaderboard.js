7faf494e0a230f33165bafbaa039c8b6e0b6d0cc
DRL\heych <D@rr3nH3lm>
2015-05-06 08:20:43 +0100

    JSON response binding to handlebars template

--- src/Paddys.Fifa.Leaderboard.Web/Content/js/leaderboard.js
+++ src/Paddys.Fifa.Leaderboard.Web/Content/js/leaderboard.js
@@ -2,14 +2,23 @@
 var Fifa = Fifa || {};
 Fifa.Leaderboard = Fifa.Leaderboard || {};
 
-
-Fifa.Leaderboard.loadAndBindLeaderboard = function () {
-    var self = this;
-
+$(document).ready(function() {
     $.get("/leaderboard", "", function (data) {
         var template = $('#leaderboard-table').html();
         var hbTemplate = Handlebars.compile(template);
 
         $(document.body).append(hbTemplate(data));
     }, "json");
-};
+});
+
+
+//Fifa.Leaderboard.loadAndBindLeaderboard = function () {
+//    var self = this;
+
+//    $.get("/leaderboard", "", function (data) {
+//        var template = $('#leaderboard-table').html();
+//        var hbTemplate = Handlebars.compile(template);
+
+//        $(document.body).append(hbTemplate(data));
+//    }, "json");
+//};
