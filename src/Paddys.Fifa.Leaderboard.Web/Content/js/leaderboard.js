63ff9e1efd246d001dd08f4a745ae4e994b0794d
DRL\heych <D@rr3nH3lm>
2015-05-12 08:02:40 +0100

    Add themed stylesheet for jquery dialog

--- src/Paddys.Fifa.Leaderboard.Web/Content/js/leaderboard.js
+++ src/Paddys.Fifa.Leaderboard.Web/Content/js/leaderboard.js
@@ -20,15 +20,3 @@ $(document).ready(function() {
     	});
     });
 });
-
-
-//Fifa.Leaderboard.loadAndBindLeaderboard = function () {
-//    var self = this;
-
-//    $.get("/leaderboard", "", function (data) {
-//        var template = $('#leaderboard-table').html();
-//        var hbTemplate = Handlebars.compile(template);
-
-//        $(document.body).append(hbTemplate(data));
-//    }, "json");
-//};
