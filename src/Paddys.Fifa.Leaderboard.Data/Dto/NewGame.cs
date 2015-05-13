b092ea297f73f0a75e6c2dc3798a38d89fd1879d
DRL\heych <D@rr3nH3lm>
2015-05-13 08:41:50 +0100

    begin to build up the calls to save the game

---
+++ src/Paddys.Fifa.Leaderboard.Data/Dto/NewGame.cs
@@ -0,0 +1,10 @@
+﻿namespace Paddys.Fifa.Leaderboard.Data.Dto
+{
+	public class NewGame
+	{
+		public int Player1Id { get; set; }
+		public int Player2Id { get; set; }
+		public int Player1Score { get; set; }
+		public int Player2Score { get; set; }
+	}
+}
---
+++ src/Paddys.Fifa.Leaderboard.Web/Modules/GameModule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Nancy;
+using Nancy.ModelBinding;
+using Paddys.Fifa.Leaderboard.Data.Dto;
+using Paddys.Fifa.Leaderboard.Data.Model;
+using Paddys.Fifa.Leaderboard.Interfaces.Games;
+
+namespace Paddys.Fifa.Leaderboard.Modules
+{
+	public class GameModule : NancyModule
+	{
+		private readonly IGameStoreService _gameStoreService;
+
+		public GameModule(IGameStoreService gameStoreService)
+		{
+			_gameStoreService = gameStoreService;
+
+			Post["/games/add"] = _ => AddGame();
+		}
+
+		private dynamic AddGame()
+		{
+			var request = this.BindAndValidate<NewGame>();
+
+			try
+			{
+				_gameStoreService.Add(new Game());
+				return true;
+			}
+			catch(Exception)
+			{
+				// TODO: logging
+				return false;
+			}
+		}
+	}
+}
